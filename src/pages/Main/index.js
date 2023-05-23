6788d5cd092a7ebd7898d88c818db6aa899b79fd
gabrielfst30 <gabrielfst30@gmail.com>
2023-05-23 19:35:53 -0300

    ft: preguiça

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,45 +1,61 @@
-import React, { useState } from 'react';
-import { FaGithub, FaPlus } from 'react-icons/fa'
-import {Container, Form, SubmitButton}  from './styles'
+import React, {useState, useCallback} from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa';
+import {Container, Form, SubmitButton} from './styles';
 
 import api from '../../services/api';
 
 export default function Main(){
 
-    const [newRepo, setNewRepo] = useState('');
+  const [newRepo, setNewRepo] = useState('');
+  const [repositorios, setRepositorios] = useState([]);
 
-    function handleSubmit(e){
-        e.preventDefault();//para não dar refresh na pag
 
-        //Puxando os repositorios
-        const response = api.get(`repos/${newRepo}`)
+  const handleSubmit = useCallback((e)=>{
+    e.preventDefault();
+
+    async function submit(){ 
+      const response = await api.get(`repos/${newRepo}`);
+  
+
+      const data = {
+        name: response.data.full_name,
+      }
+
+      
+      setRepositorios([...repositorios, data]);
+      setNewRepo('');
     }
-    
-    
-    function handleInputChange(e){
-        setNewRepo(e.target.value);
-    }
 
-    return(
-        <Container>
-            <h1>
-                <FaGithub size={25}/>
-                Meus Repositorios
-            </h1>
+    submit();
 
-        <Form onSubmit={handleSubmit}>
-            <input type='text' 
-            placeholder='Adicionar Repositorios'
-            value={newRepo}
-            onChange={handleInputChange}
-            />
+  }, [newRepo, repositorios]);
 
-            <SubmitButton>
-                <FaPlus color="#FFF" size={14}/>
-            </SubmitButton>
+  function handleinputChange(e){
+    setNewRepo(e.target.value);
+  }
 
-        </Form>   
+  return(
+    <Container>
+      
+      <h1>
+        <FaGithub size={25}/>
+        Meus Repositorios
+      </h1>
 
-        </Container>
-    )
+      <Form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        placeholder="Adicionar Repositorios"
+        value={newRepo}
+        onChange={handleinputChange}
+        />
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14}/>
+        </SubmitButton>
+
+      </Form>
+
+    </Container>
+  )
 }
